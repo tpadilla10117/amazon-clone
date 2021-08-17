9f3de6ec2c2cb121334fa12a93dfbe5d93ed9f4a
Trinidad Padilla <tpadilla101@live.com>
2021-08-16 23:16:06 -0700

    created a responsive top header

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,6 +28,30 @@
 
                     </div>
 
+                {/* Right Side: */}
+                    <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+
+                        <div className="link"> {/* using custom utility class "link" */}
+                            <p>Hello Trin Padilla</p>
+                            <p className="font-extrabold md:text-sm">Account & Lists</p>
+                        </div>
+
+                        <div className="link">
+                            <p>Returns</p>
+                            <p className="font-extrabold md:text-sm">& Orders</p>
+                        </div>
+
+                        <div className=" relative link flex items-center">
+
+                            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+
+                            <ShoppingCartIcon className="h-10" />
+                            <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                        </div>
+
+
+                    </div>
+
 
 
 
