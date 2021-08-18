e6d3bbcddd83fa935e40145e4ed6cc7ad457ffe2
Trinidad Padilla <tpadilla101@live.com>
2021-08-17 17:13:56 -0700

    will work on product feed

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -4,7 +4,7 @@
 
     function ProductFeed ( {products} ) {
         return (
-            <div>
+            <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
                 {products.map( ({ id, title, price, description, category, image }) => (
                     <Product 
                         key={id}
