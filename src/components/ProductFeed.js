8b93fa3220f24a5c2f9b2929435854dac4d752ca
Trinidad Padilla <tpadilla101@live.com>
2021-08-17 17:44:09 -0700

    completed product feed

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -5,7 +5,10 @@
     function ProductFeed ( {products} ) {
         return (
             <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-                {products.map( ({ id, title, price, description, category, image }) => (
+
+
+
+                {products.slice(0,4).map( ({ id, title, price, description, category, image }) => (
                     <Product 
                         key={id}
                         id={id}
@@ -16,6 +19,36 @@
                         image={image}
                     />
                 ))}
+
+            {/* Image banner mid feed: */}
+                <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+
+                <div className="md:col-span-2">
+                    {products.slice(4, 5).map( ({ id, title, price, description, category, image }) => (
+                        <Product 
+                            key={id}
+                            id={id}
+                            title={title}
+                            price={price}
+                            description={description}
+                            category={category}
+                            image={image}
+                        />
+                    ))}
+                </div>
+
+                {products.slice(5, products.length).map( ({ id, title, price, description, category, image }) => (
+                        <Product 
+                            key={id}
+                            id={id}
+                            title={title}
+                            price={price}
+                            description={description}
+                            category={category}
+                            image={image}
+                        />
+                    ))}
+
             </div>
         );
     };
