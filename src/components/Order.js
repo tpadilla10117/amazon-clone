2a0a61ced8ee2b261c3c12dac5629aee8758e3ca
Trinidad Padilla <tpadilla101@live.com>
2021-08-20 13:37:22 -0700

    finished styling the order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -28,6 +28,16 @@
 
                     <p className="text-sm whitespace-nowrap sm:text-xl self-end flex-1 text-right text-blue-500">{items.length} items</p>
 
+                    <p className='absolute top-2 right-2 w-40 lg:w-72 truncate text-xs whitespace-nowrap'>ORDER # {id}</p>
+
+                </div>
+
+                <div className='p-5 sm:p-10'>
+                    <div className="flex space-x-6 overflow-x-auto">
+                        {images.map( image => (
+                            <img src={image} alt="" className="h-20 object-contain sm:h-32"/> 
+                        ))}
+                    </div>
                 </div>
 
             </div>
